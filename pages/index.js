77f3cf3aa0bd952f30a6b92ec73294fe6aceb3fb
wandecleya <wandecleya@gmail.com>
2021-06-23 08:46:53 -0300

    connects cartas service to page
    # Conflicts:
    #	pages/index.js
    #	service/CartasService.js

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,40 @@ import {useEffect, useState} from "react";
 
 
 export default function Home() {
-const [respostas, setRespostas] = useState();
+const [respostas, setRespostas] = useState([]);
+const [escolhidas, setEscolhidas] = useState([]);
+const [quantidade, setQuantidade] = useState(10);
 
-const service = () => {
-  CartasService.getRespostas().then( response => setRespostas(response));
-};
 
-  useEffect(() => {
-    service();
-  }, ['']);
+  const comprarCartas = (quantidade) => {
+    CartasService.getRespostas(quantidade).then( response => {setRespostas(response), setQuantidade(1)});
+  };
 
-console.log(respostas, 'b');
+  const iniciar = () => {
+    CartasService.deleteRespostas().then(setQuantidade(10));
+  };
+
+  const escolher = (carta) => {
+    CartasService.postEscolhida(carta).then();
+  };
+
+  const verEscolhidas = () => {
+    CartasService.getEscolhidas().then(response => {setEscolhidas(response)});
+  };
+
   return (
     <div>
-      eh
-      {respostas && respostas.title}
+      <button onClick={()=>comprarCartas(quantidade)}>Comprar cartas</button>
+      <button onClick={()=>verEscolhidas()}>Ver escolhidas</button>
+      <button onClick={()=>iniciar()}>Iniciar o jogo</button>
+      <h2>Suas Cartas</h2>
+      {respostas && respostas.map((carta)=>{
+        return <button onClick={()=>escolher(carta)}>{carta.conteudo}</button>
+      })}
+      <h2>Escolhidas da Rodada</h2>
+      {escolhidas && escolhidas.map((carta)=>{
+        return <p>{carta.conteudo}</p>
+      })}
     </div>
   )
 }
--- service/CartasService.js
+++ service/CartasService.js
@@ -1,8 +1,21 @@
 import {Service} from "./api";
 
 export const CartasService = {
-  async getRespostas() {
-    const response = await  Service().get('/respostas');
+  async getRespostas(quantidade=1) {
+    const response = await  Service().get(`/respostas?quantidade=${quantidade}`);
     return response.data;
+  },
+  async getEscolhidas() {
+    const response = await  Service().get('/escolhidas');
+    return response.data;
+  },
+  async deleteEscolhidas() {
+    await  Service().delete('/deleteEscolhidas');
+  },
+  async deleteRespostas() {
+    await  Service().delete('/deleteRespostas');
+  },
+  async postEscolhida(carta) {
+    await  Service().post('/escolher', carta);
   },
 }
